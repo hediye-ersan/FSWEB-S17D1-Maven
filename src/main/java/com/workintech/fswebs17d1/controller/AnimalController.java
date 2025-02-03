14c390c231937fccb01b8cd2852752f31d49420e
hediye-ersan <hediyesnl9@gmail.com>
2025-02-03 23:24:22 +0300

    S17D1 Projesini çözer

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,45 @@
+package com.workintech.fswebs17d1.controller;
+
+import com.workintech.fswebs17d1.entity.Animal;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+    private final Map<Integer, Animal> animals = new HashMap<>();
+
+    @GetMapping
+    public List<Animal> listAll(){
+        return animals.values().stream().toList();
+    }
+
+    @GetMapping("/{id}")
+    public Animal findAnimal(@PathVariable int id){
+        return animals.get(id);
+    }
+
+    @PostMapping
+    public Animal addAnimal(@RequestBody Animal animal){
+        animals.put(animal.getId(), animal);
+        return animal;
+    }
+
+
+    @PutMapping("/{id}")
+    public Animal updateAnimal(@PathVariable int id, @RequestBody Animal animal){
+        animals.put(id, new Animal(id, animal.getName()));
+        return animals.get(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Animal delete(@PathVariable int id){
+        Animal animal = animals.get(id);
+        animals.remove(id);
+        return animal;
+    }
+}
